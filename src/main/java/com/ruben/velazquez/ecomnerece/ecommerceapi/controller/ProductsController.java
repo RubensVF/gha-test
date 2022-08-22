e089868f5f955cc27e60f1ea4102da954049d7e0
Rubén Velázquez Flores <72159360+RubensVF@users.noreply.github.com>
2022-08-22 15:23:36 -0500

    cinco

--- src/main/java/com/ruben/velazquez/ecomnerece/ecommerceapi/controller/ProductsController.java
+++ src/main/java/com/ruben/velazquez/ecomnerece/ecommerceapi/controller/ProductsController.java
@@ -14,6 +14,6 @@ public class ProductsController {
 
     @GetMapping("/")
     public List<String> getAll(){
-        return Arrays.asList("uno","dos","tres","cuatro");
+        return Arrays.asList("uno","dos","tres","cuatro","cinco");
     }
 }
